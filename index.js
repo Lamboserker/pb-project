e30a0ef7ef056768eadcccff14d0678d5b10e3ea
Lukas Lamberz <johndoe@example.comlukaslamberz96@gmail.com>
2023-05-30 14:10:23 +0200

    changes

--- index.js
+++ index.js
@@ -1,114 +1,102 @@
 // AttackSkill class
 class AttackSkill {
-    constructor(name, damage, magicCost) {
-      this.name = name;
-      this.damage = damage;
-      this.magicCost = magicCost;
-    }
+  constructor(name, damage, magicRequired) {
+    this.name = name;
+    this.damage = damage;
+    this.magicRequired = magicRequired;
   }
-  
-  // Pokemon class
-  class Pokemon {
-    constructor(name, health, magic) {
-      this.name = name;
-      this.health = health;
-      this.magic = magic;
-      this.skills = [];
-    }
-  
-    learnAttackSkill(attackSkill) {
-      this.skills.push(attackSkill);
-    }
-  
-    showStatus() {
-      console.log(`Status of ${this.name}:`);
-      console.log(`Health: ${this.health}`);
-      console.log(`Magic: ${this.magic}`);
-      console.log("-------------------");
-    }
-  
-    attack(skillIndex, targetPokemon) {
-      const attackSkill = this.skills[skillIndex];
-  
-      if (!attackSkill) {
-        console.log(`${this.name} doesn't have an attack with index ${skillIndex}!`);
-        console.log("-------------------");
-        return;
-      }
-  
-      if (this.magic < attackSkill.magicCost) {
-        console.log(`Not enough magic, ${this.name} cannot perform the attack!`);
-        console.log("-------------------");
-        return;
-      }
-  
-      console.log(`${this.name} successfully used the "${attackSkill.name}" skill!`);
-      targetPokemon.health -= attackSkill.damage;
-      this.magic -= attackSkill.magicCost;
-  
-      console.log(`${targetPokemon.name} received ${attackSkill.damage} damage.`);
-      console.log("-------------------");
-  
-      if (targetPokemon.health <= 0) {
-        console.log(`${targetPokemon.name} is defeated!`);
-        console.log("-------------------");
-      }
-    }
-  
-    getMagic() {
-      console.log(`${this.name} received 20 magic back.`);
-      this.magic += 20;
-      console.log("-------------------");
-    }
+}
+
+// Pokemon class
+class Pokemon {
+  constructor(name, health, magic) {
+    this.name = name;
+    this.health = health;
+    this.magic = magic;
+    this.skills = [];
   }
-  
-  // Creating Pokemon instances
-  let pikachu = new Pokemon("Pikachu", 120, 80);
-  let bulbasaur = new Pokemon("Bulbasaur", 95, 105);
-  let charizard = new Pokemon("Charizard", 150, 100);
-  let squirtle = new Pokemon("Squirtle", 110, 90);
-  let jigglypuff = new Pokemon("Jigglypuff", 100, 80);
-  let meowth = new Pokemon("Meowth", 90, 75);
-  let psyduck = new Pokemon("Psyduck", 100, 85);
-  let snorlax = new Pokemon("Snorlax", 200, 120);
-  
-  // Creating AttackSkill instances
-  let lightning = new AttackSkill("Lightning", 40, 30);
-  let poisonSeed = new AttackSkill("Poison Seed", 20, 20);
-  let flamethrower = new AttackSkill("Flamethrower", 50, 35);
-  let waterGun = new AttackSkill("Water Gun", 35, 25);
-  let sing = new AttackSkill("Sing", 15, 10);
-  let scratch = new AttackSkill("Scratch", 25, 15);
-  let confusion = new AttackSkill("Confusion", 30, 20);
-  let bodySlam = new AttackSkill("Body Slam", 45, 30);
-  
-  // Teaching attacks to the Pokemon
-  pikachu.learnAttackSkill(lightning);
-  bulbasaur.learnAttackSkill(poisonSeed);
-  charizard.learnAttackSkill(flamethrower);
-  squirtle.learnAttackSkill(waterGun);
-  jigglypuff.learnAttackSkill(sing);
-  meowth.learnAttackSkill(scratch);
-  psyduck.learnAttackSkill(confusion);
-  snorlax.learnAttackSkill(bodySlam);
-  
-  // Example attacks
-  pikachu.attack(0, bulbasaur);
-  bulbasaur.attack(0, pikachu);
-  charizard.attack(0, squirtle);
-  jigglypuff.attack(0, meowth);
-  psyduck.attack(0, snorlax);
-  meowth.attack(0, charizard);
-  squirtle.attack(0, jigglypuff);
-  snorlax.attack(0, psyduck);
-  
-  // Displaying status
-  pikachu.showStatus();
-  bulbasaur.showStatus();
-  charizard.showStatus();
-  squirtle.showStatus();
-  jigglypuff.showStatus();
-  meowth.showStatus();
-  psyduck.showStatus();
-  snorlax.showStatus();
-  
+
+  learnAttackSkill(attack) {
+    this.skills.push(attack);
+  }
+
+  showStatus() {
+    console.log(`${this.name} status`);
+    console.log(`Health: ${this.health}`);
+    console.log(`Magic: ${this.magic}`);
+    console.log("-------------------");
+  }
+
+  attack(attackIndex, targetPokemon) {
+    if (attackIndex >= 0 && attackIndex < this.skills.length) {
+      const attack = this.skills[attackIndex];
+      if (this.magic >= attack.magicRequired) {
+        console.log(`${this.name} launched skill '${attack.name}' successfully!`);
+        targetPokemon.health -= attack.damage;
+        this.magic -= attack.magicRequired;
+        console.log(`${targetPokemon.name} got ${attack.damage} damage`);
+        if (targetPokemon.health <= 0) {
+          console.log(`${targetPokemon.name} is killed!`);
+        }
+      } else {
+        console.log("Not enough magic, cannot launch attack!");
+      }
+    } else {
+      console.log("Invalid attack index!");
+    }
+    console.log("-------------------");
+  }
+
+  getMagic() {
+    console.log(`${this.name} got 20 magic back`);
+    this.magic += 20;
+    console.log("-------------------");
+  }
+}
+
+// Creating Pokemon and AttackSkills
+let pikachu = new Pokemon("Pikachu", 120, 80);
+let bulbasaur = new Pokemon("Bulbasaur", 95, 105);
+
+let lightning = new AttackSkill("Lightning", 40, 30);
+let poisonSeed = new AttackSkill("Poison Seed", 20, 20);
+
+// Pikachu learns attack skills
+pikachu.learnAttackSkill(lightning);
+
+// Bulbasaur learns attack skills
+bulbasaur.learnAttackSkill(poisonSeed);
+
+// Perform attacks and display statuses
+console.log("Initial statuses:");
+pikachu.showStatus();
+bulbasaur.showStatus();
+
+console.log("Performing attacks:");
+pikachu.attack(0, bulbasaur);
+bulbasaur.attack(0, pikachu);
+
+console.log("Updated statuses after attacks:");
+pikachu.showStatus();
+bulbasaur.showStatus();
+
+console.log("Performing more attacks:");
+pikachu.attack(0, bulbasaur);
+pikachu.attack(0, bulbasaur);
+
+console.log("Status after using attacks:");
+pikachu.showStatus();
+bulbasaur.showStatus();
+
+console.log("Using getMagic to replenish magic:");
+pikachu.getMagic();
+
+console.log("Performing another attack:");
+pikachu.attack(0, bulbasaur);
+
+console.log("Performing an attack with not enough magic:");
+bulbasaur.attack(0, pikachu);
+
+console.log("Final statuses:");
+pikachu.showStatus();
+bulbasaur.showStatus();
